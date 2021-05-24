d33ca0d7d8a8f8fdfaef7846fe0879a396dddc14
Hamid Khaa <hamidkhaa922@gmail.com>
2021-05-24 16:29:21 +0530

    save data function completed

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRAFXwvqQWRi8R2WKo_n3MHfl3isKnLcc",
+    authDomain: "demoproject-da6f7.firebaseapp.com",
+    projectId: "demoproject-da6f7",
+    storageBucket: "demoproject-da6f7.appspot.com",
+    messagingSenderId: "961677175443",
+    appId: "1:961677175443:web:b637311206b77d61c0bb66"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
